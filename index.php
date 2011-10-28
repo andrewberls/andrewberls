cb073e2c16a65a66f841f73029968bcfe1ad2a0a
Andrew Berls <andrew.berls@gmail.com>
2011-10-28 01:04:39 -0700

    Working on slider styling

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
 				<!--FIX TO REMOVE JS:VOID AND WORK USING NAMED ANCHORS WITHOUT JS-->
 				
 				<li><a href="javascript:void(0)"   onclick="scrollTo('portfolio')">portfolio</a></li> <!-- href home, no onclick -->
-				<li><a href="javascript:void(0)"   onclick="scrollTo('about')">about me</a></li>
+				<!--<li><a href="javascript:void(0)"   onclick="scrollTo('about')">about me</a></li>-->
 				<li><a href="javascript:void(0)"   onclick="scrollTo('contact')">get in touch</a></li>
 				<li><a href="javascript:void(0)"   onclick="scrollTo('footer')">resources</a></li>
 			</ul>
@@ -57,12 +57,7 @@
 		<div class="wrapper">
 		
 			<h1>Welcome!</h1>
-			<p>This site is still very much a work in progress, but feel free to poke around and <a href="javascript:void(0)" onclick="scrollTo('footer')">check out our resources.</a></p>
-			<p style="margin: 5px 0 15px;">This form doesn't actually do anything.</p>
-			
-			
-			
-			
+			<p>This site is still very much a work in progress, but feel free to poke around and <a href="javascript:void(0)" onclick="scrollTo('footer')">check out our resources.</a></p>			
 			
 		</div>
 	</section>
@@ -74,17 +69,34 @@
 			<div class="slider">
 				<div class="window">
 					<div class="reel">
-						<div class="frame">
-							<a href=""><img src="_images/sexinfo_large1.jpg" alt="" /></a>																			
-						</div> <!-- #FRAME NECESSARY? -->
 						
-						<div class="frame">
-							<!--<a href=""><img src="_images/bruceb_large1.jpg" alt="" /></a>-->
+						<div class="frame" style="background-color: red;">
+							<a href=""><img src="_images/sexinfo_large1.jpg" alt="" /></a>
+							<div class="text">
+								<h3>SexInfoOnline.com</h3>
+								<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+							</div>
+																								
 						</div>
 						
-						<div class="frame">
-							<!--<a href=""><img src="_images/rha_large1.jpg" alt="" /></a>-->
-						</div>										
+						<div class="frame" style="background-color: orange;">
+							<a href=""><img src="_images/bruceb_large1.jpg" alt="" /></a>
+							<div class="text">
+								<h3>Bruceb Consulting</h3>
+								<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+							</div>
+						</div>
+						
+						<div class="frame" style="background-color: yellow;">
+							<a href=""><img src="_images/rha_large1.jpg" alt="" /></a>
+							<div class="text">
+								<h3>UCSB RHA</h3>
+								<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+								
+							</div>
+						</div>	
+														
 					</div> <!-- .reel -->				
 				</div> <!-- .reel -->
 				
@@ -211,7 +223,7 @@
 		//var imageSize = $('.window').width();
 			//alert(imageSize);
 		//Adjust the image reel to its new size
-		
+		$('.window').css('overflow', 'hidden');
 	</script>
 
 <script type="text/javascript">
