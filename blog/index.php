20d3a5e2d99824a4f630d2b85d136f40b177677f
Andrew Berls <andrew.berls@gmail.com>
2011-11-09 15:30:57 -0800

    Structuring sidebar

--- blog/index.php
+++ blog/index.php
@@ -12,7 +12,7 @@
 
 <body>
 	
-	<section id="header">
+	<section id="header">		
 		<div class="wrapper">			
 			<h2>Andrew Berls <span>// blog</span></h2>
 			<ul class="nav">	
@@ -24,14 +24,33 @@
 		</div>
 	</section>
 	
-	<section id="main">
+	<section id="main">		
 		<div class="wrapper">
 			
 			<div class="sidebar">
-				<h4>About</h4>
-				<p>Description test test test lskdjf a;lwkejf lskdjf kdjf wlekrjd </p>
+				<h4>about me</h4>
+				<p>I'm currently a student at UCSB, and I love building websites. This is where I occasionally post things that amuse or interest me. I designed and wrote the software that powers this blog, and I distribute it for free over on <a href="#">GitHub.</a></p><p>If you like what you see or want to know more, you can check out my portfolio <a href="#">here</a>!</p>
 			</div>
 			
+			<div class="post">
+				<h4>Plain Latin is the best.</h4>
+				<p class="date">7 November 2011</p>
+				<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. </p><p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+			</div>
+			
+			<div class="post">
+				<h4>I use Hipster Ipsum to be ironic</h4>
+				<p class="date">7 November 2011</p>
+				<p>Readymade iphone next level vice, gentrify etsy freegan DIY blog. Seitan etsy biodiesel next level, fanny pack 8-bit brunch mixtape jean shorts. Banh mi photo booth tofu jean shorts, yr mlkshk american apparel master cleanse scenester vice mcsweeney's cred food truck. Thundercats DIY master cleanse tumblr letterpress. High life marfa fap whatever single-origin coffee lo-fi, aesthetic put a bird on it mlkshk skateboard artisan etsy letterpress blog vegan. Chambray Austin gluten-free fanny pack freegan, tumblr tofu mcsweeney's aesthetic williamsburg vice mlkshk craft beer 8-bit. Biodiesel keffiyeh fap, portland banh mi helvetica photo booth sartorial viral</p>
+			</div>
+					
+			<div class="post">
+				<h4>Cupcake ipsum. Sprinkles!</h4>
+				<p class="date">7 November 2011</p>
+				<p>Cupcake ipsum dolor sit amet. Oat cake bonbon caramels cake cupcake chocolate cake topping halvah bonbon. Cotton candy candy canes pudding oat cake biscuit icing candy canes jelly-o apple pie. Dragée carrot cake chocolate pie jelly-o cookie. Pastry fruitcake cheesecake sweet roll. Oat cake pie applicake tart pastry chupa chups. Cake topping candy wafer marzipan jujubes. Tiramisu chocolate bar pastry biscuit cotton candy chocolate cotton candy.
+Wafer sugar plum oat cake chupa chups toffee brownie tootsie roll. Gummi bears fruitcake chocolate. Apple pie brownie muffin candy marshmallow candy toffee liquorice. Halvah biscuit chupa chups gingerbread icing cookie jujubes jelly cotton candy. Macaroon croissant bonbon. Biscuit sweet roll sweet roll carrot cake jelly-o. Gummi bears wafer chocolate cake cupcake lollipop danish brownie. Pastry marshmallow pastry.</p>
+			</div>
+				
 			<div class="post">
 				<h4>Plain Latin is the best.</h4>
 				<p class="date">7 November 2011</p>
@@ -50,7 +69,9 @@
 				<p>Cupcake ipsum dolor sit amet. Oat cake bonbon caramels cake cupcake chocolate cake topping halvah bonbon. Cotton candy candy canes pudding oat cake biscuit icing candy canes jelly-o apple pie. Dragée carrot cake chocolate pie jelly-o cookie. Pastry fruitcake cheesecake sweet roll. Oat cake pie applicake tart pastry chupa chups. Cake topping candy wafer marzipan jujubes. Tiramisu chocolate bar pastry biscuit cotton candy chocolate cotton candy.
 Wafer sugar plum oat cake chupa chups toffee brownie tootsie roll. Gummi bears fruitcake chocolate. Apple pie brownie muffin candy marshmallow candy toffee liquorice. Halvah biscuit chupa chups gingerbread icing cookie jujubes jelly cotton candy. Macaroon croissant bonbon. Biscuit sweet roll sweet roll carrot cake jelly-o. Gummi bears wafer chocolate cake cupcake lollipop danish brownie. Pastry marshmallow pastry.</p>
 			</div>
-								
+				
+				
+			<a href="#header" id="top">Back to top</a>				
 		</div>		
 	</section>
 		
@@ -98,6 +119,17 @@ Wafer sugar plum oat cake chupa chups toffee brownie tootsie roll. Gummi bears f
 		</div>
 	</section>
 
+<script type="text/javascript" charset="utf-8">
+	//----- SCROLLING FOR HEADER NAVIGATION ---//
+	function scrollTo(id) {		 	    		
+		$('html,body').animate({scrollTop: $("#"+id).offset().top},'slow');		
+	}
+
+	$('#top').click(function() {		
+		scrollTo('header');
+		return false;
+	});
+</script>
 <script type="text/javascript">
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-9633739-4']);
