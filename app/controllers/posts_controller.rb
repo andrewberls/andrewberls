a5dd1fc4f29d9c6c4b1a39209d28a0b08c161751
Andrew Berls <andrew.berls@gmail.com>
2011-11-12 23:23:25 -0800

    Sutro controller structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,14 @@
 class PostsController < ApplicationController  
       
-  #----- CREATE  
-  def new # Display new record form
-    
-  end
-  
-  def create # Process new record form
-    
-  end
+  # CREATE/UPDATE/DELETE MANAGED BY SUTRO
   
   #----- READ
   def list # Main blog page - show paginated blog
-    #@books = Book.all
+    
   end
   
   def show # Show a single post
     
   end
-  
-  #----- UPDATE  
-  def edit # Display edit record form
-    
-  end
-  
-  def update # Process edit record form
-    
-  end
-  
-  #----- DELETE  
-  def destroy # Destroy record
-    
-  end
-  
+
 end
---
+++ app/controllers/sutro_controller.rb
@@ -0,0 +1,31 @@
+class SutroController < ApplicationController
+  
+  #----- GENERAL
+  def dashboard
+    
+  end
+  
+  #----- CREATE  
+  def new # Display new record form
+    
+  end
+  
+  def create # Process new record form
+    
+  end
+    
+  #----- UPDATE  
+  def edit # Display edit record form
+    
+  end
+  
+  def update # Process edit record form
+    
+  end
+  
+  #----- DELETE  
+  def destroy # Destroy record
+    
+  end
+  
+end
