6f95d465b46f475c2ff10d9bd39b0c4c07396d81
Andrew Berls <andrew.berls@gmail.com>
2011-11-19 00:06:34 -0800

    Everything else

--- db/migrate/20111113033440_create_posts.rb
+++ db/migrate/20111113033440_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string "title", :default => "", :null => false
-      t.string "body", :default => "", :null => false
+      t.string "body",  :default => "", :null => false
       t.string "tags"
       t.timestamps
     end
