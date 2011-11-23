7af3dbb7ebb980b525e0f1fe9e146ce756257ca5
Andrew Berls <andrew.berls@gmail.com>
2011-11-22 20:29:23 -0800

    Changing body from :string to :text

---
+++ db/migrate/20111123042450_change_body_string_to_text.rb
@@ -0,0 +1,11 @@
+class ChangeBodyStringToText < ActiveRecord::Migration
+  def up
+    remove_column :posts, :body
+    add_column :posts, :body, :text
+  end
+
+  def down
+    remove_column :posts, :body
+    add_column :posts, :body, :string
+  end
+end
