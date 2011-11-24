9b92bdf5f7557686718d004f41d090498c657d4c
Andrew Berls <andrew.berls@gmail.com>
2011-11-24 14:07:34 -0800

    BCrypt dependency and signup basics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+  # before_save callback calls method before password is saved
+  before_save :encrypt_password
+  
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  
+  def encrypt_password
+    # Checks if password is present and generates hash and salt using BCrypt methods
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
 end
