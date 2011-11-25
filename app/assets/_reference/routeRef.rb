170c06e735310f06a072cb326f12242641fcd5a5
Andrew Berls <andrew.berls@gmail.com>
2011-11-24 21:22:53 -0800

    Moving comments to assets file

---
+++ app/assets/_reference/routeRef.rb
@@ -0,0 +1,20 @@
+# Sample of regular route:
+#   match 'products/:id' => 'catalog#view'
+# Keep in mind you can assign values other than :controller and :action
+
+# Sample of named route:
+#   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+# This route can be invoked with purchase_url(:id => product.id)
+
+# Sample resource route (maps HTTP verbs to controller actions automatically):
+#   resources :products
+
+
+# Sample resource route within a namespace:
+#   namespace :admin do
+#     # Directs /admin/products/* to Admin::ProductsController
+#     # (app/controllers/admin/products_controller.rb)
+#     resources :products
+#   end
+
+# See how all your routes lay out with "rake routes"
--- config/routes.rb
+++ config/routes.rb
@@ -21,28 +21,4 @@ Andrewberls::Application.routes.draw do
   
   root :to => 'home#index'
   
-  
-  # match "/patients/:id" => "patients#show"
-  # The request is dispatched to the patients controller’s show action with { :id => “17” } in params.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # See how all your routes lay out with "rake routes"
 end
