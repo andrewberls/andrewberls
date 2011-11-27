6d3213f959a076629078a9ab29bf50d43bd5d681
Andrew Berls <andrew.berls@gmail.com>
2011-11-27 13:22:31 -0800

    Documentation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,16 @@
 class PostsController < ApplicationController  
-      
-  # CREATE/UPDATE/DELETE MANAGED BY SUTRO
+  
+  # Create/update/delete managed by Sutro
+  # This controller manages read-only public views
   
   #----- READ
-  def list # Main blog page - show paginated blog     
+  def list # Main blog page - will show paginated blog
+    # @page_title = "string"   
     @posts = Post.order("id DESC")
   end
   
   def show # Show a single post
+    # @page_title = "string"   
     @post = Post.find(params[:id])
   end
 
