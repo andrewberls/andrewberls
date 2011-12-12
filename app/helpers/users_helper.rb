bb80aa381f624c743d10bbb2469e45264785710f
Andrew Berls <andrew.berls@gmail.com>
2011-12-11 16:52:17 -0800

    Permission string function skeleton

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+  
+  def render_permissions(permission)
+    # Description: Permission parameter is stored as an integer. This returns the permission string associated
+    # with that number.
+    
+    case permission
+      when 0 then "Adminstrator"
+      when 1 then "Developer"     
+      when 2 then "Author"        
+      else "Something is wrong here" 
+    end
+    
+  end
+  
 end
