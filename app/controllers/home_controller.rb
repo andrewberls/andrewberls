f6d078cb040fecf164732a3cf3d5688cdfde61d9
Andrew Berls <andrew.berls@gmail.com>
2011-12-24 21:18:04 -0800

    Structuring redirects and flashes correctly

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,12 @@ class HomeController < ApplicationController
     @message = Message.new(params[:message])
     
     if @message.valid?      
-      Notifier.new_message(@message).deliver
-      redirect_to("/#contact", :notice => "Message was successfully sent.")
-    else
-      flash.now.alert = "Please fill in all fields."
-      render :index
-      #render :new
+      Notifier.new_message(@message).deliver      
+      redirect_to("/#contact", :flash => {:type => "valid", 
+                                          :msg => "Thanks! I'll get back to you as soon as possible."})     
+    else      
+      redirect_to("/#contact", :flash => {:type => "invalid", 
+                                          :msg => "Please check your fields and try again!"})      
     end
   end
   
