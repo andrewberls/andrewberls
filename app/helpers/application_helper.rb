dbba9b6b9f0ed10f43151123fdf9cce611dd86d7
Andrew Berls <andrew.berls@gmail.com>
2011-12-27 15:28:28 -1000

    Added is_admin? function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     time.strftime("%-d %b %Y")
   end
   
+  # 0: Admin
+  # 1: Developer
+  # 2: Author   
+  
+  def is_admin?(user)    
+    user.permissions == 0
+  end
+ 
 end
