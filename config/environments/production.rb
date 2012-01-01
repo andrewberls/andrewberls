68d404236bdce3cd46ea34faae05d55917b45cf1
Andrew Berls <andrew.berls@gmail.com>
2012-01-01 15:56:54 -0800

    Restructuring comment lines

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,17 +8,15 @@ Andrewberls::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  # default set to false
-  config.serve_static_assets = false
+  # Disable Rails's static asset server (Apache or nginx will already do this)  
+  config.serve_static_assets = false # Default false. Had been true, but appears to make no difference
 
   # Compress JavaScripts and CSS  
   #config.assets.css_compressor = :scss
   #config.assets.compress = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  # default set to false, changed to true
-  config.assets.compile = true
+  # Don't fallback to assets pipeline if a precompiled asset is missed  
+  config.assets.compile = true # default false (True necessary to prevent errors)
 
   # Generate digests for assets URLs
   config.assets.digest = true
