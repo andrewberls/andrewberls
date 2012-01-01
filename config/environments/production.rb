1fc73b0d9d3f034bacf58a6a2a3b2781e61839b6
Andrew Berls <andrew.berls@gmail.com>
2012-01-01 13:57:56 -0800

    Serving static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Andrewberls::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   # default set to false
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.css_compressor = :scss
