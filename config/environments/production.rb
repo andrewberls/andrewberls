fd1e0d808c6db53c22f04c387cca0c71a3696ebd
Andrew Berls <andrew.berls@gmail.com>
2012-01-01 14:15:36 -0800

    Renabling asset pipeline fallback

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,8 +17,8 @@ Andrewberls::Application.configure do
   #config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  # default set to false, was set to true
-  config.assets.compile = false
+  # default set to false, changed to true
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
