0b7127376fa4a87a48cd1ea2be713d8e92bf9a53
Andrew Berls <andrew.berls@gmail.com>
2012-01-18 14:53:13 -0800

    Adding post titles for #posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,15 @@ class PostsController < ApplicationController
 
   #----- READ
   def list # Main blog page - will show paginated blog    
-    # @page_title = "string"   
+    @page_title = "Andrew Berls | Blog"   
     # @posts = Post.where("status = 1").order("id DESC")
     
     @posts = Post.where("status = 1").paginate(:page => params[:page], :per_page => 6).order("id DESC")
   end
   
-  def show # Show a single post
-    # @page_title = "string"   
+  def show # Show a single post    
     @post = Post.find(params[:id])
+	@page_title = "Andrew Berls | " + @post.title   
   end
 
 end
