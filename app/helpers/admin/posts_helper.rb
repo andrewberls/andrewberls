6a23e8e6e031994b5e31a29e1cd44ff0a7cf7a52
Andrew Berls <andrew.berls@gmail.com>
2012-01-19 18:35:40 -0800

    Implementing framework for render_tags

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -8,4 +8,23 @@ module Admin::PostsHelper
     end
   end
   
+  def render_tags(tags, commas=true)
+    # Split the tags into an array
+    # Transform each element of the new array into a link
+    # Return the array concatenated comma-separated string of links
+    # param commas: bool determining whether or not to split result with commas
+    
+    if commas
+      raw tags.split(',').map! {|tag| render_link(tag)}.join(', ')
+    else
+      raw tags.split(',').map! {|tag| render_link(tag)}.join('')
+    end       
+  end 
+    
+  def render_link(tag)    
+    tag.strip! # Strip leading/trailing whitespace
+    loc = "/path/" + tag # This is routing specific, and subject to change
+    "<a class=\"tag\" href=\"" + loc + "\">" + tag + "</a>"
+  end
+  
 end
