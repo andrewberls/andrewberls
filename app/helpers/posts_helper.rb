9aee304d2113aabbf61571ce394d4bf83a56bf2a
Andrew Berls <andrew.berls@gmail.com>
2012-01-19 20:20:22 -0800

    Moving render_tags fxns into posts instead of admin/posts and tidying up

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,29 @@
-module PostsHelper  
+module PostsHelper
+  
+    #def render_teaser(body)
+    #  raw body[0..500] + "&hellip;"
+    #end
+  
+    def render_tags(tags, commas=true)
+    # Split the tags into an array
+    # Transform each element of the new array into a link
+    # Return the array concatenated comma-separated string of links
+    # param commas: bool determining whether or not to split result with commas
+    
+    if commas
+      raw tags.split(',').map! {|tag| render_link(tag)}.join(', ')
+    else
+      raw tags.split(',').map! {|tag| render_link(tag)}.join('')
+    end       
+  end 
+    
+  def render_link(tag)
+    # param: tag (ex: rails)
+    # returns: HTML link (ex: <a class="tag" href="rails/tag/path">Rails</a>)
+    tag.downcase! # Convert to lowercase
+    tag.strip! # Strip leading/trailing whitespace
+    loc = "/path/" + tag # TEMPORARY
+    "<a class=\"tag\" href=\"" + loc + "\">" + tag + "</a>"
+  end
+  
 end
