87a1176e06b996c924ddb1c69ab80b72535cea58
Andrew Berls <andrew.berls@gmail.com>
2012-02-11 19:28:56 -0800

    Switching to render_status_pill

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -7,5 +7,12 @@ module Admin::PostsHelper
       else "Error - Status not initialized" 
     end
   end
+  
+  def render_status_pill(status)
+    case status
+      when 0 then raw content_tag(:span, render_status(status), :class => "status-pill status-saved")
+      when 1 then raw content_tag(:span, render_status(status), :class => "status-pill status-published")
+    end
+  end
  
 end
