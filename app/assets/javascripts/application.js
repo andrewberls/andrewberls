f470bbb88fe10b181be10683d4c3ff5c2f3bdaf7
Andrew Berls <andrew.berls@gmail.com>
2012-04-29 13:33:29 -0700

    Page scrolling with hash update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,16 +2,19 @@
 //= require jquery_ujs
 
 
+//-- VENDOR PLUGINS
+//------------------------------------
+//= require jquery.scrollTo.1.4.2.min
+//= require jquery.simplemodal.1.4.2.min
+
+
 //-- SITE SCRIPTS
 //------------------------------------
+//= require scroll
 //= require contact
 //= require sutro
 //= require modal
 
-
-//-- VENDOR PLUGINS
-//------------------------------------
-//= require jquery.simplemodal.1.4.2.min
 
 
 //-- UNUSED
