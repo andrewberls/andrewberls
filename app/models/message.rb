da82251966020154c99ecbdb384f089cc8d26909
Andrew Berls <andrew.berls@gmail.com>
2012-05-02 20:57:07 -0700

    Figuring out workings of Message

--- app/models/message.rb
+++ app/models/message.rb
@@ -9,10 +9,6 @@ class Message
   validates :name, :email, :message, :presence => true
   validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => true
   
-  # This code is copied from http://matharvard.ca/posts/2011/aug/22/contact-form-in-rails-3/  
-  # Both of these methods are necessary, but I don't know what calls them,
-  # or why they exist.
-  
   def initialize(attributes = {})
     attributes.each do |name, value|
       send("#{name}=", value)
