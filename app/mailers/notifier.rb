a06e8c4e00fca42a6211ce925cb8f311fd8acacb
Andrew Berls <andrew.berls@gmail.com>
2012-05-15 22:22:09 -0700

    Fixing reply to error

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,11 +2,11 @@ class Notifier < ActionMailer::Base
     
   default :to => 'andrew.berls@gmail.com',
           :from => 'andrew@andrewberls.com'
-          #:return_path => 'andrew@andrewberls.com'
 
   def new_message(message)
     @message = message # Set an instance variable for the view (text)
-    mail(:subject  => "Message from andrewberls.com")
+    mail(:subject  => "Message from andrewberls.com",
+      :reply_to => message.email)
   end
     
 end
