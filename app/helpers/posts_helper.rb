f0495a9dfeb2708d6a17ffa2e2635f0beb4b4387
Andrew Berls <andrew.berls@gmail.com>
2012-05-19 22:10:30 -0700

    Moving code out of helpers into models and removing uneccessary getters

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,45 +1,13 @@
 module PostsHelper
   
-  def render_username(user_id)
-    User.find(user_id).full_name             
-  end
-
-
   def render_tags(tags, commas=true)
     # Transform each element of the tag array into a link
     # Return the array concatenated comma-separated string of links
     # param commas: bool determining whether or not to split result with commas
         
-    result = commas ?
-      tags.map { |t| link_to(t.name, tag_path(t.name), :class=> "tag") }.join(", ") :
-      tags.map { |t| link_to(t.name, tag_path(t.name), :class=> "tag") }.join("")
+    sep = commas ? ", " : ""
       
-    raw result
-  end 
-    
-  def has_pagebreak?(body)
-    body.include? '<break />'
-  end
-
-  def render_teaser(body)
-    # render up until the break tag if specified
-    # if encountered, break the text and add a link to full text
-    
-    if has_pagebreak?(body)                
-      # slice up until the start of the break tag
-      break_tag = '<break />'
-      endchar = body.index(break_tag) - 1         
-      body = body[0..endchar]    
-    end
-    
-    return raw body
-              
-  end
-  
-  def render_full_post(body)
-    # For show method - remove break tag and return full post
-    body.slice! '<break />'
-    raw body
+    raw tags.map { |t| link_to(t.name, tag_path(t.name), class: "tag") }.join(sep)
   end
   
 end
