eadd026e8e712c9c8d847334c2de50dd8683c8dd
Andrew Berls <andrew.berls@gmail.com>
2012-05-23 17:49:06 -0700

    Updated conversion demo

--- public/demos/rgb/js/rgb_hex.js
+++ public/demos/rgb/js/rgb_hex.js
@@ -34,42 +34,56 @@ function evaluate_rgb() {
       field.addClass("invalid");
       return false;
     } else {
-      field.removeClass("invalid")
+      // Fields OK - remove any invalid markers and perform conversion
+      field.removeClass("invalid");
+
+      var in_r = $("#in_R").val(), 
+          in_g = $("#in_G").val(), 
+          in_b = $("#in_B").val(),
+          $out_h = $("#out_hex");
+
+      var new_color = "#" + rgbToHex(in_r, in_g, in_b);
+      
+      $out_h.val(new_color);
+      $("#previewBox").css("background-color", new_color); 
+      $("#previewText").css("color", new_color);
     }
   });
 
-  var in_r = $("#in_R").val(), 
-      in_g = $("#in_G").val(), 
-      in_b = $("#in_B").val(),
-      $out_h = $("#out_hex");
-
-  var new_color = "#" + rgbToHex(in_r, in_g, in_b);
-  
-  $out_h.val(new_color);
-  $("#previewBox").css("background-color", new_color); 
-  $("#previewText").css("color", new_color);
 }
 
 function evaluate_hex() {
-  var in_h = $("#in_hex").val();
+
+  var $in_h = $("#in_hex"),       // JQuery input object
+       in_h = $("#in_hex").val(); // Actual hex input value
 
   if (in_h.charAt(0) === "#") {
-    in_h = in_h.substr(1);
+    // Trim off '#' if present
+      in_h = in_h.substr(1);
+    }
+
+  if (!in_h || [3,6].indexOf(in_h.length) === -1) {
+    // Catch blank value or values not of length 3 or 6
+    $in_h.addClass("invalid");
+    return false;
+  } else {
+    $in_h.removeClass("invalid");
+
+    if (in_h.length == 3) {
+      // Dirty hack to coerce 3-digit number into 6
+      in_h = in_h[0] + in_h[0] +
+             in_h[1] + in_h[1] +
+             in_h[2] + in_h[2];
+    }
+
+    $("#out_R").val(hexToVal(in_h, 0, 2));
+    $("#out_G").val(hexToVal(in_h, 2, 4));
+    $("#out_B").val(hexToVal(in_h, 4, 6));
+    
+    $("#previewBox").css("background-color", "#" + in_h); 
+    $("#previewText").css("color", "#" + in_h);
   }
 
-  if (in_h.length == 3) {
-    // Dirty hack to coerce 3-digit number into 6
-    in_h = in_h[0] + in_h[0] +
-           in_h[1] + in_h[1] +
-           in_h[2] + in_h[2];
-  }
-
-  $("#out_R").val(hexToVal(in_h, 0, 2));
-  $("#out_G").val(hexToVal(in_h, 2, 4));
-  $("#out_B").val(hexToVal(in_h, 4, 6));
-  
-  $("#previewBox").css("background-color", "#" + in_h); 
-  $("#previewText").css("color", "#" + in_h);
 }
 
 
