ea51e008340f8664c0e385907977a13bf52ac551
Andrew Berls <andrew.berls@gmail.com>
2012-05-28 21:28:27 -0700

    Changing accept to only on before_filter

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   layout 'sutro'    
   
   # Check authentication for on public-facing actions
-  before_filter :check_auth, :accept => [:new, :manage]  
+  before_filter :check_auth, :only => [:new, :manage]  
   
   
   #----- CREATE
