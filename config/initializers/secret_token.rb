14985d4ef3bc28def3f4cf9c7319b13cf9662bdd
Andrew Berls <andrew.berls@gmail.com>
2012-06-08 00:25:10 -0700

    Key info

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,3 @@
-# Be sure to restart your server when you modify this file.
+# Dummy generated key provided for dev / clones
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 Andrewberls::Application.config.secret_token = '3cac547b290ec3f65f808068bc263e4700e7100013b6c5401c9cbbac9e41e86467a0f26f73dbea9508f906914ced1e5733d65d59551e28afccf7dbd6f23f21f1'
