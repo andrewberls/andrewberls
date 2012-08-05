a0418968c2d52cf3518f3055f8b7804a3c475f2f
Andrew Berls <andrew.berls@gmail.com>
2012-08-05 16:50:12 -0700

    Using builtin current_page? instead of action_request

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,9 @@
 module ApplicationHelper
-  
+
   def render_sutro_time(time)
     # Format a string timestamp for overviews and public blog posts
     # Ex: 7 Oct 2011
     time.strftime("%-d %b %Y")
   end
-     
-  def action_request
-    # Returns a formatted string of the requested controller/action pair
-    # Ex "users#manage"
-    params[:controller] + "#" + params[:action]
-  end
- 
+
 end
