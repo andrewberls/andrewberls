53c441d6e68ec2497115d2f274e719cccbc5597a
Andrew Berls <andrew.berls@gmail.com>
2012-08-05 17:16:00 -0700

    Cleaning up users controller

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,75 +1,70 @@
 class Admin::UsersController < ApplicationController
-  
-  layout 'sutro'    
-  
-  # Check authentication for on public-facing actions
+
+  layout 'sutro'
+
   before_filter :must_be_logged_in
-  
-  
+
+
   #----- CREATE
   def new
     check_admin_privileges(admin_posts_path)
-    # Display form to add a new user
     @user = User.new
   end
 
   def create
-    @user = User.new(params[:user])       
+    @user = User.new(params[:user])
     # A little unsure of why this is explicitly necessary
     @user.permissions = params[:user][:permissions]
 
-    if @user.save      
-      flash[:success] = "User created successfully"    
-      redirect_to manage_users_path     
-    else      
-      # Save failed - render the signup form again
+    if @user.save
+      flash[:success] = "User created successfully"
+      redirect_to manage_users_path
+    else
       render :new
     end
 
   end
-  
-  
+
+
   #----- READ
   def manage
-    check_admin_privileges(admin_posts_path)    
+    check_admin_privileges(admin_posts_path)
     # Sort all users for Admin/Dev/Author list order
     @users = User.order("permissions ASC")
   end
-  
+
   def show
     @user = User.find(params[:id])
     @posts = Post.where(:user_id => @user.id)
                  .paginate(:page => params[:page], :per_page => 10).order("id DESC")
   end
-  
-  
+
+
   #----- UPDATE
-  def edit # Display edit record form
-    # Form fields prefilled with values from instance variable passed to view
+  def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
-    
+
     # A little unsure of why this is explicitly necessary
     @user.permissions = params[:user][:permissions]
-      
+
     if @user.update_attributes(params[:user])
       flash[:success] = "User updated successfully"
       redirect_to manage_users_path
     else
-      # Update fails - redisplay the form
       render :edit
     end
   end
-  
-  
+
+
   #----- DELETE
-  def destroy # Destroy record
-    User.find(params[:id]).destroy # Don't need an instance variable
-    flash[:success] = "User deleted succesfully" 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted succesfully"
     redirect_to manage_users_path
   end
-  
+
 end
