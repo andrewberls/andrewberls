7d8e4d312cd78db00dd691f6cb27dd077cb57362
Andrew Berls <andrew.berls@gmail.com>
2012-08-29 14:42:43 -0700

    Updating rgb demo

--- public/demos/rgb/js/page_swap.js
+++ public/demos/rgb/js/page_swap.js
@@ -1,22 +1,24 @@
+$form = $('#convert-form');
+
 $(function() {
   // Load RGB convert initially
-  $('#convert-form').load('forms/rgb_form.html');
+  $form.load('forms/rgb_form.html');
 
   // Handle navbar display
   $('#navbar li a').click(function() {
-    $('#navbar li a').removeClass('current');   
+    $('#navbar li a').removeClass('current');
     $(this).addClass('current');
   });
 
   // Load appropriate form on navbar click
   $("#loadRGB").click(function() {
-    $('#convert-form').load('forms/rgb_form.html', function() {
+    $form.load('forms/rgb_form.html', function() {
       evaluate_rgb();
     });
   });
 
   $("#loadHex").click(function() {
-    $('#convert-form').load('forms/hex_form.html', function() {
+    $form.load('forms/hex_form.html', function() {
       evaluate_hex();
     });
   });
--- public/demos/rgb/js/rgb_hex.js
+++ public/demos/rgb/js/rgb_hex.js
@@ -1,24 +1,24 @@
 //-- UTILITY CONVERSION METHODS
 //------------------------------------
-function toHex(a) { 
-  if (a === null) { 
-    return "00"; 
-  } 
-  a = parseInt(a, 10); 
-  if (a === 0 || isNaN(a)) { 
-    return "00"; 
-  } 
-  a = Math.max(0, a); 
-  a = Math.min(a, 255); 
-  a = Math.round(a); 
+function toHex(a) {
+  if (a === null) {
+    return "00";
+  }
+  a = parseInt(a, 10);
+  if (a === 0 || isNaN(a)) {
+    return "00";
+  }
+  a = Math.max(0, a);
+  a = Math.min(a, 255);
+  a = Math.round(a);
   return "0123456789ABCDEF".charAt((a - a % 16) / 16) + "0123456789ABCDEF".charAt(a % 16);
 }
 
-function rgbToHex(r, g, b) { 
+function rgbToHex(r, g, b) {
   return toHex(r) + toHex(g) + toHex(b);
-} 
+}
 
-function hexToVal(h, start, end) { 
+function hexToVal(h, start, end) {
   return parseInt(h.substring(start, end), 16);
 }
 
@@ -29,7 +29,7 @@ function evaluate_rgb() {
 
   $.each([$("#in_R"), $("#in_G"), $("#in_B")], function(i, field) {
     // Validate fields and prevent bad conversion
-    
+
     if (!field.val() || field.val().length > 3) {
       field.addClass("invalid");
       return false;
@@ -37,15 +37,15 @@ function evaluate_rgb() {
       // Fields OK - remove any invalid markers and perform conversion
       field.removeClass("invalid");
 
-      var in_r = $("#in_R").val(), 
-          in_g = $("#in_G").val(), 
+      var in_r = $("#in_R").val(),
+          in_g = $("#in_G").val(),
           in_b = $("#in_B").val(),
           $out_h = $("#out_hex");
 
       var new_color = "#" + rgbToHex(in_r, in_g, in_b);
-      
+
       $out_h.val(new_color);
-      $("#previewBox").css("background-color", new_color); 
+      $("#previewBox").css("background-color", new_color);
       $("#previewText").css("color", new_color);
     }
   });
@@ -54,8 +54,8 @@ function evaluate_rgb() {
 
 function evaluate_hex() {
 
-  var $in_h = $("#in_hex"),       // JQuery input object
-       in_h = $("#in_hex").val(); // Actual hex input value
+  var $in_h = $("#in_hex"), // JQuery input object
+       in_h = $in_h.val();  // Actual hex input value
 
   if (in_h.charAt(0) === "#") {
     // Trim off '#' if present
@@ -79,8 +79,8 @@ function evaluate_hex() {
     $("#out_R").val(hexToVal(in_h, 0, 2));
     $("#out_G").val(hexToVal(in_h, 2, 4));
     $("#out_B").val(hexToVal(in_h, 4, 6));
-    
-    $("#previewBox").css("background-color", "#" + in_h); 
+
+    $("#previewBox").css("background-color", "#" + in_h);
     $("#previewText").css("color", "#" + in_h);
   }
 
@@ -89,22 +89,24 @@ function evaluate_hex() {
 
 //-- CLICK HANDLERS
 //------------------------------------
-$(function () { 
-  $("#convert-form").on('click', '#rgbConvert', function() {
+$form = $("#convert-form");
+
+$(function () {
+  $form.on('click', '#rgbConvert', function() {
     evaluate_rgb();
     return false;
   });
 
-  $("#convert-form").on('click', '#hexConvert', function() {
+  $form.on('click', '#hexConvert', function() {
     evaluate_hex();
     return false;
   });
-    
+
   // $("#clone").click(function () {
   //   $("#in_G, #in_B").attr("value", $("#in_R").val());
-  //   evaluate_rgb();   
+  //   evaluate_rgb();
   // });
-  
+
 });
 
 
