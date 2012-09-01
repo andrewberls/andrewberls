811d522398a05df511c9653c20b62a790fcbbc52
Andrew Berls <andrew.berls@gmail.com>
2012-08-31 23:24:08 -0700

    Method name was incorrectly overriding attribute and causing some confusion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,43 +1,43 @@
 class User < ActiveRecord::Base
-  
+
   has_many :posts, :dependent => :destroy
-  
+
   attr_accessible :full_name, :email, :password, :password_confirmation, :permissions
   attr_accessor :password, :password_confirmation
   before_save :encrypt_password # callback calls encrypt method before password is saved
-    
+
   validates :full_name, presence: true, length: { maximum: 50 }
-  
-  validates :email, presence: true,                    
+
+  validates :email, presence: true,
                     uniqueness: { case_sensitive: false }
-    
+
   validates_presence_of :password, :on => :create
   validates_confirmation_of :password
 
-  # Permission utility methods 
+  # Permission utility methods
 
   def is_admin?
-    self[:permissions] == 0
-  end
-  
-  def is_developer?
-    self[:permissions] == 1
-  end
-  
-  def is_author?
-    self[:permissions] == 2
+    permissions == 0
   end
 
-  def permissions
+  def is_developer?
+    permissions == 1
+  end
+
+  def is_author?
+    permissions == 2
+  end
+
+  def permission_str
     # Return the permission string associated with the users permission number.
-    
+
     case self[:permissions]
       when 0 then "Adminstrator"
-      when 1 then "Developer"     
+      when 1 then "Developer"
       when 2 then "Author"
     end
   end
-  
+
   def encrypt_password
     # Checks if password is present and generates hash and salt using BCrypt methods
     if password.present?
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
       self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
     end
   end
-  
+
   def self.authenticate(email, password)
     user = find_by_email(email)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
