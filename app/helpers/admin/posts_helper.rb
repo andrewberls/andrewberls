6557719ddf84aee341067b6606de28ba5fb34825
Andrew Berls <andrew.berls@gmail.com>
2012-09-19 17:18:46 -0700

    Refactoring table helpers

--- app/helpers/admin/posts_helper.rb
+++ app/helpers/admin/posts_helper.rb
@@ -1,18 +1,12 @@
 module Admin::PostsHelper
-  
+
   def render_status(status)
-    case status
-      when 0 then "Saved"
-      when 1 then "Published"                  
-      else "Error - Status not initialized" 
-    end
+    (status == 0) ? "Saved" : "Published"
   end
-  
+
   def render_status_pill(status)
-    case status
-      when 0 then raw content_tag(:span, render_status(status), :class => "status-pill status-saved")
-      when 1 then raw content_tag(:span, render_status(status), :class => "status-pill status-published")
-    end
+    status_class = (status == 0) ? "status-saved" : "status-published"
+    raw content_tag(:span, render_status(status), :class => "status-pill #{status_class}")
   end
- 
+
 end
