30ec35ccbe656c84afa66c52bf5748bc41cc9b10
Andrew Berls <andrew.berls@gmail.com>
2014-11-09 13:35:07 -0800

    Hack: Add $.browser shim

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,8 @@
 //= require jquery
 //= require jquery_ujs
 //
+//= require jquery_browser_shim
+//
 //= require jquery.scrollTo.1.4.2.min
 //= require jquery.simplemodal.1.4.2.min
 //= require jquery.mailcheck.min
